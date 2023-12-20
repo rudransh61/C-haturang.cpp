79df7d9125ef97ecdf910c83245cc98d86bc223d
Rudransh Bhardwaj <aarti19830@gmail.com>
2023-12-20 18:23:45 +0530

    how pawn moves stored

--- main.cpp
+++ main.cpp
@@ -24,32 +24,36 @@ int castlingRight;
 int enpassantsquare;
 
 // all possible squares for pieces
-std::vector<std::vector<int>> whitePawnsMoves;
-std::vector<std::vector<int>> whiteKnightsMoves;
 std::vector<std::vector<int>> whiteBishopsMoves;
 std::vector<std::vector<int>> whiteRooksMoves;
 std::vector<std::vector<int>> whiteQueensMoves;
 std::vector<std::vector<int>> whiteKingMoves;
 
-void allCoveredSquareWhitePawns(char board[8][8]){
+std::vector<std::vector<int>> allCoveredSquareWhitePawns(int board[8][8]){
+	
+	std::vector<std::vector<int>> whitePawnsMoves;	
 	for(int i=0;i<8;i++){
 		for(int j=0;j<8;j++){
 			// check whether it is a pawn or not	
-			if(board[i][j]=='1'){
+			if(board[i][j]==1){
 				// check for up and up+up square is covered or not
-				if(board[i-1][j]=='0'){
+				if(board[i-1][j]==0){
 					whitePawnsMoves.push_back({i-1,j});
 				}
-				if(board[i-2][j]=='0'){
+				if(board[i-2][j]==0){
 					whitePawnsMoves.push_back({i-2,j});
 				}
 			}
 		}
 	}
+
+	return whitePawnsMoves;
 }
 
-void allCoveredSquareWhiteKnight(char board[8][8]){
+std::vector<std::vector<int>> allCoveredSquareWhiteKnights(int board[8][8]){
 	
+	std::vector<std::vector<int>> whiteKnightsMoves;
+		
 	// for knight moves
 	int X[8] = {2 , 1 , -1 , -2 , -2 , -1 , 1 , 2};
 	int Y[8] = { 1, 2 , 2 , 1,-1,-2,-2,-1 };
@@ -57,7 +61,7 @@ void allCoveredSquareWhiteKnight(char board[8][8]){
 	for(int i=0;i<8;i++){
 		for(int j=0;j<8;j++){
 			// check whether it is a knight or not
-			if(board[i][j]=='2'){
+			if(board[i][j]==2){
 				
 				// check for every move
 				for(int k=0;k<8;k++){
@@ -73,22 +77,24 @@ void allCoveredSquareWhiteKnight(char board[8][8]){
 			}
 		}
 	}
+	return whiteKnightsMoves;
+
 }
 
 
-bool isBlackMated(char board[8][8]){
+bool isBlackMated(int board[8][8]){
 
 
 	return 0;
 
 }
 
-bool isWhiteMated(char board[8][8]){
+bool isWhiteMated(int board[8][8]){
 	return 0;
 }
 
 
-float evaluation(char board[8][8]){
+float evaluation(int board[8][8]){
 
 	if(isWhiteMated(board)){
 		return 1000;
@@ -113,7 +119,7 @@ float evaluation(char board[8][8]){
 }
 
 
-void printboard(char board[8][8]){
+void printboard(int board[8][8]){
 	for(int i=0;i<8;i++){
 		for(int j=0;j<8;j++){
 			std::cout<<board[i][j]<<" ";
@@ -124,7 +130,7 @@ void printboard(char board[8][8]){
 
 
 int main(){
-	char board[8][8]={
+	int board[8][8]={
 		{-4,-2,-3,-5,-6,-3,-2,-4},
 		{-1,-1,-1,-1,-1,-1,-1,-1},
 		{ 0, 0, 0, 0, 0, 0, 0, 0},
@@ -134,20 +140,24 @@ int main(){
 		{ 1, 1, 1, 1, 1, 1, 1, 1},
 		{ 4, 2 ,3, 5, 6, 3, 2, 4}	
 	};
-
+	
+	//print the board
 	printboard(board);
 
 
 	// all white pawn moves
-	allCoveredSquareWhitePawns(board);
-	for(any : whitePawnsMoves){
-		for(squares : any){
-			std::cout<<squares[0]<<","squares[1]<<std::endl;
-		}
+	std::vector<std::vector<int>> whitePawnsMoves = allCoveredSquareWhitePawns(board);
+	for(const auto& any : whitePawnsMoves){
+		std::cout<<"Pawn : "<<any[0]<<","<<any[1]<<std::endl;
+	}
+	
+	std::vector<std::vector<int>> whiteKnightsMoves = allCoveredSquareWhiteKnights(board);
+	for(const auto& any : whiteKnightsMoves){
+		std::cout<<"Knight : "<<any[0]<<","<<any[1]<<std::endl;
 	}
 	
 	// evaluation of board
-	float eval = evaluation(board);
+	//float eval = evaluation(board);
 	
-	std::cout<<eval<<std::endl;
+	//std::cout<<eval<<std::endl;
 }
